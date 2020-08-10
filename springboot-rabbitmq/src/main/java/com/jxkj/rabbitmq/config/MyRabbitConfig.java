fc25939d3d12c3d5210474d204e06ab1d81799a8
yysimple <5325809+yysimple@user.noreply.gitee.com>
2020-08-10 23:27:13 +0800

    新增：测试消息由交换机到queue失败后的回调

--- springboot-rabbitmq/src/main/java/com/jxkj/rabbitmq/config/MyRabbitConfig.java
+++ springboot-rabbitmq/src/main/java/com/jxkj/rabbitmq/config/MyRabbitConfig.java
@@ -1,9 +1,15 @@
 package com.jxkj.rabbitmq.config;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * 项目: springboot-rabbitmq
@@ -16,8 +22,51 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyRabbitConfig {
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * 当初始化配置的时候就触发
+     */
+    @PostConstruct
+    public void initRabbitTemplate() {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            /**
+             * 功能描述: 在消息顺利抵达bloker的时候 ack 为 true
+             * @author WuChengxing
+             * @date 2020/8/8
+             * @param correlationData 消息的唯一id ， 需要在发送方自己生成，在这里就可以接受到
+             * @param ack
+             * @param s 失败的原因
+             * @return void
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String s) {
+                System.out.println("correlationData = " + correlationData + ", ack = " + ack + ", s = " + s);
+            }
+        });
+
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            /**
+             * 功能描述: 消息抵达队列的回调(只有投递失败的时候才能触发)
+             * @author WuChengxing
+             * @date 2020/8/8
+             * @param message 投递失败的详细信息
+             * @param code 回复的状态码
+             * @param text 回复的文本内容
+             * @param exchange 使用的交换机
+             * @param routeKey 使用的路由键
+             * @return void
+             */
+            @Override
+            public void returnedMessage(Message message, int code, String text, String exchange, String routeKey) {
+                System.out.println("message = " + message + ", code = " + code + ", text = " + text + ", exchange = " + exchange + ", routeKey = " + routeKey);
+            }
+        });
     }
 }
--- springboot-rabbitmq/src/main/java/com/jxkj/rabbitmq/controller/MqController.java
+++ springboot-rabbitmq/src/main/java/com/jxkj/rabbitmq/controller/MqController.java
@@ -2,11 +2,14 @@ package com.jxkj.rabbitmq.controller;
 
 import com.jxkj.rabbitmq.entity.MyRabbitMqUser;
 import com.jxkj.rabbitmq.entity.SysUser;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * 项目: springboot-rabbitmq
@@ -27,7 +30,7 @@ public class MqController {
         for (int i = 0; i < num; i++) {
             if (i % 2 == 0) {
                 MyRabbitMqUser myRabbitMqUser = new MyRabbitMqUser(1L + i, "hahah", "123456", 19);
-                rabbitTemplate.convertAndSend("hello-java-exchange", "hello.java", myRabbitMqUser);
+                rabbitTemplate.convertAndSend("hello-java-exchange", "hello.java", myRabbitMqUser, new CorrelationData(UUID.randomUUID().toString()));
             }else {
                 SysUser sysUser = new SysUser(1L + 1, "haha"+ i, "123"+ i);
                 rabbitTemplate.convertAndSend("hello-java-exchange", "hello.java", sysUser);
